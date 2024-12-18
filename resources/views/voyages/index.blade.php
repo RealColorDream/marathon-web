e22ecab9d90605ae7b738d9a4d184e84794088ec
Gomez Gonzalez Enzo <enzo_gomezgonzalez@ens.univ-artois.fr>
2024-12-18 23:33:28 +0100

    :bug: fixing the css

--- resources/views/voyages/index.blade.php
+++ resources/views/voyages/index.blade.php
@@ -10,7 +10,7 @@
     <div class="uk-container uk-margin-large-top">
         {{-- Message de succès/erreurs --}}
         @if(session('success'))
-            <div class="uk-alert-success" uk-alert>
+            <div>
                 <a class="uk-alert-close" uk-close></a>
                 <p>{{ session('success') }}</p>
             </div>
@@ -23,14 +23,12 @@
         @endif
 
         {{-- Section des voyages publics --}}
-        <h1 class="uk-heading-line"><span>Nos Voyages Publics</span></h1>
-        <div class="uk-grid-small uk-child-width-1-2@s uk-child-width-1-3@m" uk-grid>
-            <h1><span>Nos Voyages Publics</span></h1>
-
+        <h1><span>Nos Voyages Publics</span></h1>
+        <div>
             <div class="grid-voyage">
                 @forelse($voyagesPublics as $voyage)
                     <div>
-                        <x-voyage-card :voyage="$voyage" />
+                        <x-voyage-card :voyage="$voyage"/>
                     </div>
                 @empty
                     <p>Aucun voyage public disponible pour le moment.</p>
@@ -47,10 +45,11 @@
                 <div class="uk-grid-small uk-child-width-1-2@s uk-child-width-1-3@m" uk-grid>
                     @forelse($voyagesPrives as $voyage)
                         <div>
-                            <x-voyage-card :voyage="$voyage" />
+                            <x-voyage-card :voyage="$voyage"/>
 
                             {{-- Formulaire d'activation --}}
-                            <form action="{{ route('voyages.activate', $voyage->id) }}" method="POST" class="uk-margin-top">
+                            <form action="{{ route('voyages.activate', $voyage->id) }}" method="POST"
+                                  class="uk-margin-top">
                                 @csrf
                                 @method('PATCH')
                                 <button type="submit" class="uk-button uk-button-secondary uk-width-1-1">
--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/normalize.css', 'resources/css/app.css', 'resources/js/app.js',
+            input: ['resources/css/normalize.css', 'resources/css/app.css', 'resources/js/app.js', 'resources/css/voyage.css', 'resources/css/auth.css',
                 'resources/css/test-vite.css', 'resources/css/profil.css', 'resources/js/test-vite.js'],
             refresh: true,
         }),
