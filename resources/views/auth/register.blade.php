552fd6d72976b328e22a2c78114212bf14934f78
Bastien <bastien.lang11@gmail.com>
2024-12-18 14:31:28 +0100

    update register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,23 +1,31 @@
 @extends("templates.app")
 
 @section('content')
-<h2 class="connection-title">Créer mon compte</h2>
-<div class="login-form">
-    <img src="" alt="Image utilisateur"/>
-    <h3>Je me crée un compte</h3>
-    <hr/>
-<form action="{{route("register")}}" method="post">
-    @csrf
-    <label for="name">Nom d'utilisateur</label>
-    <input type="text" name="name" required placeholder="Name" /><br />
-    <label for="email">E-mail</label>
-    <input type="email" name="email" required placeholder="Email" /><br />
-    <label for="password">Mot de passe</label>
-    <input type="password" name="password" required placeholder="password" /><br />
-    <label for="password_confirmation">Vérification mot de passe</label>
-    <input type="password" name="password_confirmation" required placeholder="password" /><br />
-    <input type="submit" value="Valider"/><br />
-</form>
-Déjà un compte ? <a href="{{route("login")}}">Connectez vous</a>
-</div>
+<section class="register">
+    <h2 class="connection-title">Créer mon compte</h2>
+    <div class="login-form">
+        <i class='bx bx-user'></i>
+    <form action="{{route("register")}}" method="post">
+        @csrf
+        <div>
+            <label for="name">Nom d'utilisateur</label>
+            <input type="text" name="name" required placeholder="Name" />
+        </div>
+        <div>
+            <label for="email">E-mail</label>
+            <input type="email" name="email" required placeholder="Email" />
+        </div>
+        <div>
+            <label for="password">Mot de passe</label>
+            <input type="password" name="password" required placeholder="password" />
+        </div>
+        <div>
+            <label for="password_confirmation">Vérification mot de passe</label>
+            <input type="password" name="password_confirmation" required placeholder="password" />
+        </div>
+        <input type="submit" value="Valider"/>
+    </form>
+    Déjà un compte ? <a href="{{route("login")}}">Connectez vous</a>
+    </div>
+</section>
 @endsection
