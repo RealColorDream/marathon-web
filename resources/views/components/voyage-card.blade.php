54104b71d348b8da5a6acb9485053097d125f3f4
A. Jakubiak <contact@jakubiak.fr>
2024-12-18 15:13:01 +0100

    :sparkles: Added a voyage-card component

---
+++ resources/views/components/voyage-card.blade.php
@@ -0,0 +1,12 @@
+@props([
+    /** @var \mixed */
+    'voyage'
+])
+
+<div {{ $attributes->class(['uk-card uk-card-default uk-card-hover uk-card-body']) }}>
+    <img src="{{ $voyage->visuel }}" alt="{{ $voyage->titre }}" class="uk-width-1-1 uk-margin-small-bottom">
+    <h3 class="uk-card-title">{{ $voyage->titre }}</h3>
+    <p>{{ $voyage->resume }}</p>
+    <p><strong>Continent : </strong>{{ $voyage->continent }}</p>
+    <a href="{{ route('voyages.show', $voyage->id) }}" class="uk-button uk-button-primary">Voir le voyage</a>
+</div>
--- resources/views/journeys/index.blade.php
+++ resources/views/journeys/index.blade.php
@@ -13,13 +13,7 @@
         <div class="uk-grid-small uk-child-width-1-2@s uk-child-width-1-3@m" uk-grid>
             @foreach($voyages as $voyage)
                 <div>
-                    <div class="uk-card uk-card-default uk-card-hover uk-card-body">
-                        <img src="{{ $voyage->visuel }}" alt="{{ $voyage->titre }}" class="uk-width-1-1 uk-margin-small-bottom">
-                        <h3 class="uk-card-title">{{ $voyage->titre }}</h3>
-                        <p>{{ $voyage->resume }}</p>
-                        <p><strong>Continent : </strong>{{ $voyage->continent }}</p>
-                        <a href="{{ route('voyages.show', $voyage->id) }}" class="uk-button uk-button-primary">Voir le voyage</a>
-                    </div>
+                    <x-voyage-card :voyage="$voyage"/>
                 </div>
             @endforeach
         </div>
