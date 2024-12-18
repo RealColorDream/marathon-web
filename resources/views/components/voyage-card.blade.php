f1d6a64de3dc7a48c350eac8e79c5fca295e40a8
Gomez Gonzalez Enzo <enzo_gomezgonzalez@ens.univ-artois.fr>
2024-12-18 20:16:37 +0100

    :construction: :lipstick: integration des voyages

--- resources/views/components/voyage-card.blade.php
+++ resources/views/components/voyage-card.blade.php
@@ -4,8 +4,9 @@
 ])
 
 <div {{ $attributes->class(['uk-card uk-card-default uk-card-hover uk-card-body']) }}>
+    <h2>{{$voyage->titre}}</h2>
     <img src="{{ $voyage->visuel }}" alt="{{ $voyage->titre }}">
-    <h3 class="uk-card-title">{{ $voyage->titre }}</h3>
+    {{--<h3 class="uk-card-title">{{ $voyage->titre }}</h3>--}}
     <p>{{ $voyage->resume }}</p>
     <p><strong>Continent : </strong>{{ $voyage->continent }}</p>
     <a href="{{ route('voyages.show', $voyage->id) }}" class="uk-button uk-button-primary">Voir le voyage</a>
