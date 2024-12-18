b906f88b468d1ad64f87ac188017bca998ef7f08
rayane_rousseau <rousseau.rayane@gmail.com>
2024-12-18 22:08:21 +0100

    :memo: Bouton "Activer" fonctionnel

--- resources/views/voyages/index.blade.php
+++ resources/views/voyages/index.blade.php
@@ -25,43 +25,43 @@
         {{-- Section des voyages publics --}}
         <h1 class="uk-heading-line"><span>Nos Voyages Publics</span></h1>
         <div class="uk-grid-small uk-child-width-1-2@s uk-child-width-1-3@m" uk-grid>
-        <h1><span>Nos Voyages Publics</span></h1>
+            <h1><span>Nos Voyages Publics</span></h1>
 
-        <div class="grid-voyage">
-            @forelse($voyagesPublics as $voyage)
-                <div>
-                    <x-voyage-card :voyage="$voyage" />
-                </div>
-            @empty
-                <p>Aucun voyage public disponible pour le moment.</p>
-            @endforelse
-        </div>
-
-        {{-- Section des voyages non publiés (privés) --}}
-        @if(Auth::check())
-            <h2 class="uk-heading-line uk-margin-top"><span>Vos Voyages Non Publiés</span></h2>
-            <a href="{{ route('voyages.create') }}" class="uk-button uk-button-primary uk-margin-bottom">
-                Créer un voyage
-            </a>
-
-            <div class="uk-grid-small uk-child-width-1-2@s uk-child-width-1-3@m" uk-grid>
-                @forelse($voyagesPrives as $voyage)
+            <div class="grid-voyage">
+                @forelse($voyagesPublics as $voyage)
                     <div>
                         <x-voyage-card :voyage="$voyage" />
-
-                        {{-- Formulaire d'activation --}}
-                        <form action="{{ route('voyages.activate', $voyage->id) }}" method="POST" class="uk-margin-top">
-                            @csrf
-                            @method('PATCH')
-                            <button type="submit" class="uk-button uk-button-secondary uk-width-1-1">
-                                Activer
-                            </button>
-                        </form>
                     </div>
                 @empty
-                    <p>Vous n'avez pas de voyages non publiés pour le moment.</p>
+                    <p>Aucun voyage public disponible pour le moment.</p>
                 @endforelse
             </div>
-        @endif
-    </div>
+
+            {{-- Section des voyages non publiés (privés) --}}
+            @if(Auth::check())
+                <h2 class="uk-heading-line uk-margin-top"><span>Vos Voyages Non Publiés</span></h2>
+                <a href="{{ route('voyages.create') }}" class="uk-button uk-button-primary uk-margin-bottom">
+                    Créer un voyage
+                </a>
+
+                <div class="uk-grid-small uk-child-width-1-2@s uk-child-width-1-3@m" uk-grid>
+                    @forelse($voyagesPrives as $voyage)
+                        <div>
+                            <x-voyage-card :voyage="$voyage" />
+
+                            {{-- Formulaire d'activation --}}
+                            <form action="{{ route('voyages.activate', $voyage->id) }}" method="POST" class="uk-margin-top">
+                                @csrf
+                                @method('PATCH')
+                                <button type="submit" class="uk-button uk-button-secondary uk-width-1-1">
+                                    Activer
+                                </button>
+                            </form>
+                        </div>
+                    @empty
+                        <p>Vous n'avez pas de voyages non publiés pour le moment.</p>
+                    @endforelse
+                </div>
+            @endif
+        </div>
 @endsection
