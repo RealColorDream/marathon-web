2d5a77177f4a40d81362364817b120b095974e98
rayane_rousseau <rousseau.rayane@gmail.com>
2024-12-18 14:44:50 +0100

    :memo: Fix et améliorations

--- routes/web.php
+++ routes/web.php
@@ -3,28 +3,22 @@
 use App\Http\Controllers\VoyageController;
 use Illuminate\Support\Facades\Route;
 
+// Accueil
 Route::get('/', [VoyageController::class, 'index'])->name('accueil');
 
-Route::get('/contact', function () {
-    return view('contact');
-})->name("contact");
+// Contact
+Route::get('/contact', fn() => view('contact'))->name("contact");
 
-/*Route::get('/test-vite', function () {
-    return view('test-vite');
-})->name("test-vite");*/
+// Tableau de bord
+Route::get('/dashboard', fn() => view('dashboard'))->name("dashboard")->middleware('auth');
 
-Route::get('/home', function () {
-    return view('dashboard');
-})->name("home")->middleware('auth');
+// Redirection de /home vers /dashboard
+Route::get('/home', fn() => redirect()->route('dashboard'));
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->name("dashboard")->middleware('auth');
-
-Route::get('/journeys', [VoyageController::class, 'index'])->name('journeys.index');
-
+// Voyages
+Route::get('/voyages', [VoyageController::class, 'index'])->name('voyages.index');
 Route::get('/voyages/{id}', [VoyageController::class, 'show'])->name('voyages.show');
 
+// Création et stockage des voyages
 Route::get('/voyages/create', [VoyageController::class, 'create'])->name('voyages.create');
 Route::post('/voyages', [VoyageController::class, 'store'])->name('voyages.store');
-
