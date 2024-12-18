c7bf50f81d565e7b2afda6fafa86738a7960c160
Gomez Gonzalez Enzo <enzo_gomezgonzalez@ens.univ-artois.fr>
2024-12-18 16:12:19 +0100

    :sparkles: debut CRUD

---
+++ resources/views/etapes/edit.blade.php
@@ -0,0 +1,22 @@
+@extends('templates.app')
+
+@section('title', 'Accueil')
+
+@section('content')
+    <div class="uk-container uk-margin-large-top">
+        <h1 class="uk-heading-line"><span>Etapes</span></h1>
+
+        <div class="uk-grid-small uk-child-width-1-2@s uk-child-width-1-3@m" uk-grid>
+            <div>
+                <div class="uk-card uk-card-default uk-card-hover uk-card-body">
+                    @foreach ($medias as $media)
+                        <img src="{{ $media->url }}" alt="{{ $etape->titre }}"
+                             class="uk-width-1-1 uk-margin-small-bottom">
+                    @endforeach
+                    <h3 class="uk-card-title">{{ $etape->titre }}</h3>
+                    <p>{{ $etape->resume }}</p>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
