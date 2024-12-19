32647337db28512315f0afc8caef482d7cf5b280
Gomez Gonzalez Enzo <enzo_gomezgonzalez@ens.univ-artois.fr>
2024-12-19 07:10:52 +0100

    :bug: fixed the route not being named correctly

--- routes/web.php
+++ routes/web.php
@@ -35,5 +35,5 @@ Route::get('/etape/{id}/edit', [EtapeController::class, 'edit'])->name('etape.ed
 Route::put('/etape/{id}', [EtapeController::class, 'update'])->name('etape.update');
 Route::delete('/etape/{id}', [EtapeController::class, 'destroy'])->name('etape.destroy');
 
-Route::get('/voyages/{id}/etapes/create', [EtapeController::class, 'create'])->name('etapes.create');
-Route::post('/voyages/{id}/etapes', [EtapeController::class, 'store'])->name('etapes.store');
+Route::get('/voyages/{id}/etape/create', [EtapeController::class, 'create'])->name('etapes.create');
+Route::post('/voyages/{id}/etape', [EtapeController::class, 'store'])->name('etapes.store');
