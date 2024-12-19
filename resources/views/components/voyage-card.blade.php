43d1e9ae7c739cb5d141a85be613ca2b20764bce
rayane_rousseau <rousseau.rayane@gmail.com>
2024-12-19 12:24:31 +0100

    :memo: Retirer la description

--- resources/views/components/voyage-card.blade.php
+++ resources/views/components/voyage-card.blade.php
@@ -6,8 +6,7 @@
 <div {{ $attributes->class(['uk-card uk-card-default uk-card-hover uk-card-body']) }}>
     <h2>{{$voyage->titre}}</h2>
     <img src="{{ $voyage->visuel }}" alt="{{ $voyage->titre }}">
-    {{--<h3 class="uk-card-title">{{ $voyage->titre }}</h3>--}}
-    <p>{{ $voyage->resume }}</p>
+    {{--<p>{{ $voyage->resume }}</p> <!-- Résumé supprimé -->--}}
     <p><strong>Continent : </strong>{{ $voyage->continent }}</p>
     <a href="{{ route('voyages.show', $voyage->id) }}" class="uk-button uk-button-primary">Voir le voyage</a>
 </div>
