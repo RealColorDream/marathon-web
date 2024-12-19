9a0e3a60d9e015151bcf3bb4fbda7756546128fa
Gomez Gonzalez Enzo <enzo_gomezgonzalez@ens.univ-artois.fr>
2024-12-19 15:36:22 +0100

    :bug: fixing vite

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/normalize.css', 'resources/css/app.css', 'resources/js/app.js', 'resources/css/voyage.css', 'resources/css/auth.css',"resources/css/about.css",
-                'resources/css/test-vite.css', 'resources/css/profil.css', 'resources/js/test-vite.js'],
+            input: ['resources/css/normalize.css', 'resources/css/app.css', 'resources/js/app.js', 'resources/css/voyage.css', 'resources/css/show-etape.css', 'resources/css/show-voyage.css', 'resources/css/404.css',
+                'resources/css/auth.css', 'resources/css/accueil.css', 'resources/css/profil.css', 'resources/css/about.css'],
             refresh: true,
         }),
     ],
