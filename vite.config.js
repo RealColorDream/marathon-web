44655b0ecb37564138cc777e43626d554fd19373
Clement Goustiaux <clement_goustiaux@ens.univ-artois.fr>
2024-12-19 14:49:59 +0100

    🦺 Virer test vite

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
     plugins: [
         laravel({
             input: ['resources/css/normalize.css', 'resources/css/app.css', 'resources/js/app.js', 'resources/css/voyage.css', 'resources/css/auth.css',
-                'resources/css/test-vite.css', 'resources/css/profil.css', 'resources/js/test-vite.js'],
+                'resources/css/profil.css', 'resources/js/test-vite.js'],
             refresh: true,
         }),
     ],
