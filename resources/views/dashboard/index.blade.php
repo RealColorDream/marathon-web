2967bfca1e3d354b3c013b8d3ecfa151fa288285
rayane_rousseau <rousseau.rayane@gmail.com>
2024-12-19 15:53:00 +0100

    :rocket: Améliorations et corrections dashboard

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -7,41 +7,34 @@
 
 @section("content")
 
-<section class="profile">
-    <img src="{{Vite::asset('resources/images/van.svg')}}" alt="van" class="van">
-    <div class="app-profile">
-        <h2 class="user-likes-title">Voyages likés : </h2>
-        <div class="user-profile">
-            <div class="user-details">
-                <h1>{{ $user->name }}</h1>
-                <p class="mail">{{ $user->email }}</p>
+    <section class="profile">
+        <img src="{{Vite::asset('resources/images/van.svg')}}" alt="van" class="van">
+        <div class="app-profile">
+            <h2 class="user-likes-title">Voyages likés : </h2>
+            <div class="user-profile">
+                <div class="user-details">
+                    <h1>{{ $user->name }}</h1>
+                    <p class="mail">{{ $user->email }}</p>
+                </div>
+                <img src="{{ $user->avatar }}" alt="Avatar de {{ $user->name }}" class="user-avatar">
             </div>
-            <img src="{{ $user->avatar }}" alt="Avatar de {{ $user->name }}" class="user-avatar">
         </div>
-    </div>
-    <div class="voyage-likes">
+        <div class="voyage-likes">
+            @if($user->likes->isEmpty())
+                <p>Vous n'avez liké aucun voyage.</p>
+            @else
+                <div class="voyage-likes">
+                    @foreach($user->likes as $voyage)
+                        <div class="voyage-box">
+                            <!-- Titre du voyage -->
+                            <h3 class="voyage-title">{{ $voyage->titre }}</h3>
 
-        @if($user->likes->isEmpty())
-            <p>Vous n'avez liké aucun voyage.</p>
-        @else
-            <div class="voyage-likes">
-                @foreach($user->likes as $voyage)
-                    <div class="voyage-box">
-                        <!-- Image du voyage -->
-                        <img src="{{ $voyage->visuel }}" alt="Visuel de {{ $voyage->titre }}">
-
-                        <!-- Titre du voyage -->
-                         <h3>{{ $voyage->titre }}</h3>
-
-                        <!-- Résumé -->
-                        <p>{{ $voyage->resume }}</p>
-
-                        <!-- Auteur du voyage -->
-                        <small>Édité par : {{ $voyage->editeur->name }}</small>
-                    </div>
-                @endforeach
-            </div>
-        @endif
-    </div>
-</section>
+                            <!-- Image du voyage -->
+                            <img src="{{ $voyage->visuel }}" alt="Visuel de {{ $voyage->titre }}">
+                        </div>
+                    @endforeach
+                </div>
+            @endif
+        </div>
+    </section>
 @endsection
