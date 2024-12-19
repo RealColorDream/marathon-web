f42731a2543a1e0adefcf6b3cd0b56a9586435dd
Bastien <bastien.lang11@gmail.com>
2024-12-19 02:22:12 +0100

    dashboard fonts + van

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -14,7 +14,7 @@
         <div class="user-profile">
             <div class="user-details">
                 <h1>{{ $user->name }}</h1>
-                <p>{{ $user->email }}</p>
+                <p class="mail">{{ $user->email }}</p>
             </div>
             <img src="{{ $user->avatar }}" alt="Avatar de {{ $user->name }}" class="user-avatar">
         </div>
@@ -31,7 +31,7 @@
                         <img src="{{ $voyage->visuel }}" alt="Visuel de {{ $voyage->titre }}">
 
                         <!-- Titre du voyage -->
-                        <h3>{{ $voyage->titre }}</h3>
+                         <h3>{{ $voyage->titre }}</h3>
 
                         <!-- Résumé -->
                         <p>{{ $voyage->resume }}</p>
