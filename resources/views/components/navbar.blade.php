93a13460f5aca70b8ded5d102a329f3382d132ea
Gomez Gonzalez Enzo <enzo_gomezgonzalez@ens.univ-artois.fr>
2024-12-19 07:39:11 +0100

    :sparkles: add the logo

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,26 @@
+<nav {{ $attributes }}>
+    <ul>
+
+        <li><a href="{{ route('accueil') }}"><img src="{{ Vite::asset('resources/images/logo.svg') }}" alt="logo"></a></li>
+        <li><a href="{{ route('voyages.index') }}">Voyages</a></li>
+        <!--<form action="{{ route('voyages.index') }}" method="GET" class="uk-search uk-search-default">
+            <div class="uk-search-icon-flip" uk-search-icon>
+                <input class="uk-search-input" type="search" name="search" placeholder="Search...">
+                <button class="uk-search-icon-flip" type="submit" uk-search-icon>Recherche</button>
+            </div>
+        </form>-->
+        @auth
+            <li><a href="{{ route('dashboard') }}">{{ Auth::user()->name }}</a></li>
+            <li>
+                <a href="{{ route('logout') }}" onclick="document.getElementById('logout').submit(); return false;">
+                    Logout
+                </a>
+                <form id="logout" action="{{ route('logout') }}" method="post" style="display: none;">
+                    @csrf
+                </form>
+            </li>
+        @else
+            <li><a class="navbar-button" href="{{ route('login') }}"> <i class='bx bx-user'></i> Connexion</a></li>
+        @endauth
+    </ul>
+</nav>
