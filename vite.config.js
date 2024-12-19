d35529b1a8f8ce32fb8683e22560a632fd3b0c6f
Gomez Gonzalez Enzo <enzo_gomezgonzalez@ens.univ-artois.fr>
2024-12-19 15:36:22 +0100

    :bug: fixing vite

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
     plugins: [
         laravel({
             input: ['resources/css/normalize.css', 'resources/css/app.css', 'resources/js/app.js', 'resources/css/voyage.css', 'resources/css/show-etape.css', 'resources/css/show-voyage.css', 'resources/css/404.css',
-                'resources/css/auth.css', 'resources/css/accueil.css', 'resources/css/profil.css'],
+                'resources/css/auth.css', 'resources/css/accueil.css', 'resources/css/profil.css', 'resources/css/about.css'],
             refresh: true,
         }),
     ],
